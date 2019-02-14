c6edde1eed12b38e8b36b2a0d9a337f1b72ab673
Sam Sale <sam.sale@sky.com>
2019-02-13 16:25:58 -0800

    Tidied up navigation bar code

--- valet-app/app/javascript/components/Elements/NavigationBar.js
+++ valet-app/app/javascript/components/Elements/NavigationBar.js
@@ -3,39 +3,36 @@ import PropTypes from "prop-types"
 import {Navbar, NavItem, Icon} from 'react-materialize'
 
 class NavigationBar extends React.Component {
-
-
-createNavBarItems = (userDetails) => {
-  console.log("fun",userDetails);
-  if (userDetails.logged_in === false){
-    return(
-      //If you are not signed in
+  createNavBarItems = (userDetails) => {
+    if (userDetails.logged_in === false){
+      return(
+        //If you are not signed in
+        <React.Fragment>
+          <NavItem>View Valet Services</NavItem>
+          <NavItem href='/users/sign_in'>Sign In</NavItem>
+          <NavItem href='/registration'>Sign Up</NavItem>
+        </React.Fragment>
+      )
+    //if the user is logged in and the is a valet
+    }else if (userDetails.logged_in && userDetails.is_valet) {
+      return(
+        <React.Fragment>
+          <NavItem>Dashboard</NavItem>
+          <NavItem href='/'>View Jobs</NavItem>
+          <NavItem href='/users/sign_out'>Sign Out</NavItem>
+        </React.Fragment>
+      )
+      //if the user is logged in and is a customer
+    }else if (userDetails.logged_in && userDetails.is_valet === false){
+      return(
       <React.Fragment>
-        <NavItem>View Valet Services</NavItem>
-        <NavItem href='/users/sign_in'>Sign In</NavItem>
-        <NavItem href='/registration'>Sign Up</NavItem>
-      </React.Fragment>
-    )
-  //if the user is logged in and the is a valet
-  }else if (userDetails.logged_in && userDetails.is_valet) {
-    return(
-      <React.Fragment>
-        <NavItem>Dashboard</NavItem>
-        <NavItem href='/'>View Jobs</NavItem>
+        <NavItem href='/'>Create New Order</NavItem>
+        <NavItem href='/'>View My Orders</NavItem>
         <NavItem href='/users/sign_out'>Sign Out</NavItem>
       </React.Fragment>
-    )
-    //if the user is logged in and is a customer
-  }else if (userDetails.logged_in && userDetails.is_valet === false){
-    return(
-    <React.Fragment>
-      <NavItem href='/'>Create New Order</NavItem>
-      <NavItem href='/'>View My Orders</NavItem>
-      <NavItem href='/users/sign_out'>Sign Out</NavItem>
-    </React.Fragment>
-    )
+      )
+    }
   }
-}
 
   render () {
     const {userDetails: {props}} = this.props

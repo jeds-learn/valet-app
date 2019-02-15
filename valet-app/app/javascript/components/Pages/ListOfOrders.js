8b90ec83b42798acc4f3a5ff45f75626ea7c4da1
Sam Sale <sam.sale@sky.com>
2019-02-14 18:35:33 -0800

    Changed name of componenets

--- valet-app/app/javascript/components/Pages/ListOfOrders.js
+++ valet-app/app/javascript/components/Pages/ListOfOrders.js
@@ -1,50 +1,55 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom'
-import {Row, Table, Button, Input} from 'react-materialize'
+import {BrowserRouter as Router, Route } from 'react-router-dom'
+import {Table, Icon, Button, Col, Row, Input} from 'react-materialize'
+
 
 class ListOfOrders extends React.Component {
-
   render () {
     return (
+      <div className="container">
+        <h1>My Bookings</h1>
 
-      <div>
-        <Row className="container">
-          <Input s={12} label="Search Available Valets" validate defaultValue=' ' />
-        </Row>
-
-        <Table className="container">
-          <thead>
-            <tr>
-              <th data-field="id">Name of user</th>
-              <th data-field="name">Business Name</th>
-              <th data-field="name">Valet Company</th>
-              <th data-field="name"></th>
+        <Table striped bordered centered>
+          <thead >
+            <tr >
+              <th data-field="Booking">Booking</th>
+              <th data-field="Location">Location</th>
+              <th data-field="Arrival Time">Arrival Time</th>
+              <th data-field="Price">Price</th>
+              <th data-field="Status">Status</th>
+              <th data-field="Details"></th>
             </tr>
           </thead>
 
           <tbody>
-            <tr>
-              <td>Name 1</td>
-              <td>Business 1</td>
-              <td>Valet 1</td>
-              <td><Link className="waves-effect waves-light btn register-btn" to={'/book_valet/details'}>Select</Link></td>
-
+            <tr >
+              <td >Current booking</td>
+              <td >Location</td>
+              <td >2:30 PM</td>
+              <td >Price</td>
+              <td >Pending</td>
+              <td><Button waves='light'>Details</Button></td>
             </tr>
-            <tr>
-              <td>Name 1</td>
-              <td>Business 2</td>
-              <td>Valet 2</td>
-              <td><Link className="waves-effect waves-light btn register-btn" to={'/book_valet/details'}>Select</Link></td>
+            <tr >
+              <td >Previous Booking</td>
+              <td >Location</td>
+              <td >11:45 AM</td>
+              <td >Price</td>
+              <td >Canceled</td>
+              <td><Button waves='light'>Details</Button></td>
             </tr>
-            <tr>
-              <td>name 1</td>
-              <td>Business 3</td>
-              <td>Valet 3</td>
-              <td><Link className="waves-effect waves-light btn register-btn" to={'/book_valet/details'}>Select</Link></td>
+            <tr >
+              <td >Last Years Booking</td>
+              <td >Location</td>
+              <td >1:00 PM</td>
+              <td >Price</td>
+              <td >Complete</td>
+              <td><Button waves='light'>Details</Button></td>
             </tr>
           </tbody>
         </Table>
+
       </div>
     )
   }

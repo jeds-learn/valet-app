f8c307498c58c10c667d68e03255db529b2eb6a6
stevenkulp <sdkulp@gmail.com>
2019-02-19 10:48:35 -0800

    added time fields.  Still must be connected to filter bar.

--- valet-app/app/javascript/components/Pages/ValetDashboard.js
+++ valet-app/app/javascript/components/Pages/ValetDashboard.js
@@ -1,22 +1,38 @@
 import React from "react"
 import PropTypes from "prop-types"
 import {BrowserRouter as Router, Route } from 'react-router-dom'
-import {Table, Icon, Button, Col, Row, Input, CollapsibleItem, Collapsible, Modal, DateTime} from 'react-materialize'
-
+import {Table, Icon, Button, Col, Row, Input, CollapsibleItem, Collapsible, Modal} from 'react-materialize'
+import DateTime from 'react-datetime'
+import moment from 'moment'
 
 class ValetDashboard extends React.Component {
+
+//Do not need all these object keys. We could get rid of some
+//Need to add filter bar to the left next to time field
+  state ={
+    orderDetails:{
+    start_date_time: null,
+    end_date_time: null,
+    total_price: null,
+  },
+    valetDetails: []
+  }
+
+// this field is to search by date. Need to connect it to filter bar
+  getStartTime = (date) => {
+    let {start} = this.state
+    this.setState({start:date})
+  }
+
 
   render () {
     return (
       <div className="container">
         <h1>Valet Dashboard</h1>
-
         <Row>
-          <Input s={12} label="Generic Field.  Meant to allow valet to select start time" validate defaultValue=' ' />
-        </Row>
-
-        <Row>
-          <Input s={12} label="Search For An Order" validate defaultValue=' ' />
+            <Icon left>access_alarm</Icon><DateTime className="col s5" defaultValue={"Drop Off Time"} timeConstraints={ {minutes: { step: 15 }}} onChange={this.getStartTime} name="start-date" />
+            <Icon left>access_alarm</Icon><DateTime className="col s5" defaultValue={"Collection Time"} timeConstraints={ {minutes: { step: 15 }}} onChange={this.getStartTime} name="start-date" />
+            <Input s={12} label="Search For An Order" validate defaultValue='filter bar' />
         </Row>
 
         <Table striped bordered centered>

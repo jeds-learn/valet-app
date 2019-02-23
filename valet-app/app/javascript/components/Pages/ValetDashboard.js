8a1928eb4c0f1b895247ba41778bd37d0347f1de
Sam Sale <sam.sale@sky.com>
2019-02-22 16:09:20 -0800

    Fixed backend for valet dashboard orders

--- valet-app/app/javascript/components/Pages/ValetDashboard.js
+++ valet-app/app/javascript/components/Pages/ValetDashboard.js
@@ -2,11 +2,13 @@
 //For Dan and Enrique to Create
 import React from "react"
 import PropTypes from "prop-types"
-import {BrowserRouter as Router, Route } from 'react-router-dom'
-import {Table, Icon, Button, Col, Row, Input, T, Card, CardTitle, CollapsibleItem, Collapsible, Modal} from 'react-materialize'
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import {Table, Icon, Button, Col, Row, Input, T, Card, CardTitle, CollapsibleItem, Collapsible, Modal, ProgressBar} from 'react-materialize'
 import DateTime from 'react-datetime'
 import moment from 'moment'
 import { Chart } from "react-google-charts"
+import 'babel-polyfill';
+
 
 // variables for Orders Chart
 const data = [
@@ -48,32 +50,87 @@ const revOptions = {
 const rows = [[1, 3], [2, 5], [3, 1]];
 
 class ValetDashboard extends React.Component {
-
-//Do not need all these object keys. We could get rid of some
-//Need to add filter bar to the left next to time field
-// details button currently inoperorable.  Must be able to take changes to the order.
   state ={
-    orderDetails:{
-    start_date_time: null,
-    end_date_time: null,
-    total_price: null,
-  },
-    valetDetails: []
+    loading: true,
+    orders: []
   }
 
-// this field is to search by date. Need to connect it to filter bar
-  getStartTime = (date) => {
-    let {start} = this.state
-    this.setState({start:date})
+componentDidMount = () => {
+  this.getOrders()
+}
+
+  convertedTime = (time) => {
+    return moment.utc(time).format('LT')
+  }
+  convertedDate = (time) => {
+    return moment.utc(time).format('l')
+  }
+  getOrders = async () => {
+    event.preventDefault()
+    let response = await fetch('/valet/orders.json', {
+      method:"GET",
+      headers: {"Content-Type": "application/json"},
+    })
+    let json = await response.json()
+    await this.setState({orders: json, loading: false})
+  }
+
+  updateOrderStatus = async (event) => {
+    event.preventDefault()
+    console.log(event.currentTarget);
+    // let response = await fetch('/valet/orders.json', {
+    //   method:"GET",
+    //   headers: {"Content-Type": "application/json"},
+    // })
+    // let json = await response.json()
+    // await this.setState({orders: json, loading: false})
   }
 
   render () {
+    if (this.state.loading === true) {
+      return <Row>
+              <Col s={12}>
+                <ProgressBar />
+              </Col>
+            </Row>
+    }
+    console.log(this.props);
     return (
       <div className="container">
         <h1>Valet Dashboard</h1>
         <div>
           <h3>Recent Orders</h3>
         </div>
+
+        <Table striped bordered centered>
+          <thead >
+            <tr >
+              <th data-field="Arrival Date">Arrival Date</th>
+              <th data-field="Arrival Time">Arrival Time</th>
+              <th data-field="Customer Name">Customer Name</th>
+              <th data-field="Make">Make</th>
+              <th data-field="Color">License Plate</th>
+              <th data-field="Status">Status</th>
+              <th data-field="Details"></th>
+            </tr>
+          </thead>
+          <tbody>
+          {this.state.orders.map((order, index) => {
+            return(
+              <tr key={index} id={order.id}>
+              <td>{this.convertedDate(order.start_date)}</td>
+              <td>{this.convertedTime(order.start_time)}</td>
+              <td>{order.customer_first_name} {order.customer_last_name}</td>
+              <td>{order.vehicle_make}</td>
+              <td>{order.vehicle_license_plate}</td>
+              <td>{order.order_status}</td>
+              <td><Button id={order.id} onClick={() => {$('#details').modal('open')}} waves='light'>Update Status</Button></td>
+              </tr>
+            )
+          })}
+          </tbody>
+        </Table>
+
         <div>
         <Modal className='modal-class'
           id="details"
@@ -81,8 +138,8 @@ class ValetDashboard extends React.Component {
           actions={
             <div>
               <Row>
-                <Col s={2} offset="s2">
-                  <Button floating flat modal="close" waves="light" icon='thumb_up' tooltip="Client Arrived" className='deep-purple lighten-2 ' ></Button>
+                <Col s={2} offset="s1">
+                  <Button floating flat modal="close" waves="light" icon='thumb_up' tooltip="Client Arrived" onClick={this.updateOrderStatus} className='deep-purple lighten-2 ' ></Button>
                 </Col>
                 <Col s={2}>
                   <Button floating flat modal="close" waves="light" icon='local_parking' tooltip="Car Parked" className='blue lighten-2'></Button>
@@ -96,57 +153,8 @@ class ValetDashboard extends React.Component {
               </Row>
             </div>
           }>
-          <p>
-           Please update vehicle status:
-          </p>
-        </Modal></div>
 
-        <Table striped bordered centered>
-          <thead >
-            <tr >
-              <th data-field="Arrival Time">Arrival Time</th>
-              <th data-field="Departure Time">Departure Time</th>
-              <th data-field="Customer Name">Customer Name</th>
-              <th data-field="Location">Location</th>
-              <th data-field="Make">Make</th>
-              <th data-field="Color">Color</th>
-              <th data-field="Status">Status</th>
-              <th data-field="Details"></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr >
-              <td >2:30PM</td>
-              <td >10:30PM</td>
-              <td >Daniel</td>
-              <td >slot A1</td>
-              <td >Honda</td>
-              <td >Silver</td>
-              <td >Pending</td>
-              <td><Button waves='light' onClick={()=> {$('#details').modal('open')}}>Details</Button></td>
-            </tr>
-            <tr >
-              <td >2:30PM</td>
-              <td >10:30PM</td>
-              <td >Daniel</td>
-              <td >slot A1</td>
-              <td >Honda</td>
-              <td >Silver</td>
-              <td >Pending</td>
-              <td><Button waves='light' onClick={()=> {$('#details').modal('open')}}>Details</Button></td>
-            </tr>
-            <tr >
-              <td >2:30PM</td>
-              <td >10:30PM</td>
-              <td >Daniel</td>
-              <td >slot A1</td>
-              <td >Honda</td>
-              <td >Silver</td>
-              <td >Pending</td>
-              <td><Button waves='light' onClick={()=> {$('#details').modal('open')}}>Details</Button></td>
-            </tr>
-          </tbody>
-        </Table>
+        </Modal></div>
         <div>
           <h3>Overview: Order Status</h3>
         </div>
--- valet-app/app/models/order.rb
+++ valet-app/app/models/order.rb
@@ -1,10 +1,17 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :valet, class_name: "User", foreign_key: "valet_id"
+  belongs_to :customer, class_name: "User", foreign_key: "user_id"
+  belongs_to :vehicle, class_name: "Vehicle", foreign_key: "vehicle_id"
   delegate :cost_per_hour, to: :valet
   delegate :company_name, to: :valet, prefix:true
   delegate :address, to: :valet, prefix:true
   delegate :city, to: :valet, prefix:true
   delegate :state, to: :valet, prefix:true
   delegate :zip, to: :valet, prefix:true
+  delegate :make, to: :vehicle, prefix:true
+  delegate :license_plate, to: :vehicle, prefix:true
+  delegate :first_name, to: :customer, prefix:true
+  delegate :last_name, to: :customer, prefix:true
+
 end

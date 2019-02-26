dc9663df7484f38e6b9b05f4a06251c39eea50fe
Sam Sale <sam.sale@sky.com>
2019-02-26 09:06:56 -0800

    Changed 404 page

--- valet-app/app/javascript/components/Pages/PageNotFound.js
+++ valet-app/app/javascript/components/Pages/PageNotFound.js
@@ -1,20 +1,19 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-import {Card, CardTitle, Col, Row} from 'react-materialize'
+import {Link} from 'react-router-dom'
 
 //Images used for 404 PageNotFound.
-import Car from '../../../assets/img/car2.jpg'
+import Car from '../images/icon-404.png'
 
 class PageNotFound extends React.Component {
   render () {
     return (
       <div className = "container error-404">
-        <h1> 404 ERROR!!!</h1>
-        <h3> Your page is not here. We are working on this, we promise</h3>
+        <h2>Page Not Found</h2>
         <div>
-          <img src={Car} alt="Cartoon image of red broken car"/>
+          <img height="300px" src={Car} alt="Cartoon image of red broken car"/>
         </div>
+        Return to <Link to="/">home</Link>
       </div>
     )
   }
